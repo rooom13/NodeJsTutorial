5146cb3b93e6c7e249623b4df933f8b6bb83eab1
roomm13 <romanreypedrero@gmail.com>
2017-12-27 13:40:10 +0100

    Express

--- src/ExpressFramework/server2.js
+++ src/ExpressFramework/server2.js
@@ -1,15 +1,50 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express 	= require('express');
 
+var bodyParser 	= require('body-parser');
+
+var cookieParser = require('cookie-parser')
+
+
+var fs = require("fs");
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
+var app = express();
+app.use(cookieParser());
 app.use(express.static('public'));
+
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+//app.use(multer({ dest: '/tmp/'}));
+var upload = multer({dest: '/tmp'});
 
 //Set dir
 app.get('/index.htm', function (req, res) {
    res.sendFile(__dirname + "/" + "index.htm");
+})
+
+
+app.post('/file_upload', function (req, res) {
+   console.log(req.files.file.name);
+   console.log(req.files.file.path);
+   console.log(req.files.file.type);
+   var file = __dirname + "/" + req.files.file.name;
+   
+   fs.readFile( req.files.file.path, function (err, data) {
+      fs.writeFile(file, data, function (err) {
+         if( err ){
+            console.log( err );
+            }else{
+               response = {
+                  message:'File uploaded successfully',
+                  filename:req.files.file.name
+               };
+            }
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+   });
 })
 
 //Form data obtaining using GET
@@ -37,6 +72,7 @@ app.post('/process_post', urlencodedParser, function (req, res) {
 	
 app.get('/', function (req, res) {
    res.send('Hello Woorld');
+console.log("Cookies: ", req.cookies)
 })
 
 var server = app.listen(8081, function () {
